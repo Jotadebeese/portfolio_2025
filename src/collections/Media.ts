9f8ee6fdf542babe92ff0a262b9e586ef2f2c4c6
Juan <jotadebeese@gmail.com>
2025-03-11 21:51:12 +0700

    Added Vercel BLOB to Payload

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,16 +1,56 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 export const Media: CollectionConfig = {
-  slug: 'media',
+  slug: "media",
   access: {
     read: () => true,
   },
+
   fields: [
     {
-      name: 'alt',
-      type: 'text',
+      name: "alt",
+      type: "text",
       required: true,
     },
   ],
-  upload: true,
-}
+  upload: {
+    adminThumbnail: "thumbnail",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: 300,
+        position: "centre",
+        generateImageName: ({ height, sizeName, extension, width }) => {
+          return `custom-${sizeName}-${height}-${width}.${extension}`;
+        },
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre",
+        generateImageName: ({ height, sizeName, extension, width }) => {
+          return `custom-${sizeName}-${height}-${width}.${extension}`;
+        },
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        height: undefined,
+        position: "centre",
+        generateImageName: ({
+          height,
+          sizeName,
+          extension,
+          width,
+          originalName,
+        }) => {
+          return `${originalName}-${sizeName}-${height}-${width}.${extension}`;
+        },
+      },
+    ],
+    mimeTypes: ["image/*"],
+    displayPreview: true,
+  },
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,18 @@
 // storage-adapter-import-placeholder
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -22,18 +23,23 @@ export default buildConfig({
   },
   collections: [Users, Media],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.DATABASE_URI || "",
     },
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
-})
+});
