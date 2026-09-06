6340eea7abde010739d73dcadda306f0822aec4e
Juan <jotadebeese@gmail.com>
2026-09-06 14:42:45 +0700

    chore: add posthog ui_host for managed reverse proxy

--- src/components/providers/PostHogProvider.tsx
+++ src/components/providers/PostHogProvider.tsx
@@ -20,6 +20,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       posthog.init(posthogKey, {
         api_host:
           process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        ui_host: "https://us.posthog.com",
         person_profiles: "identified_only",
         capture_pageview: false, 
         capture_pageleave: true,
